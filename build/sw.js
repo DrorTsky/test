ad07e1926515ac7d0bb49538ac933f119bdd6ac2
Dror Tartakovsky <drortartak@gmail.com>
2020-11-02 12:45:58 +0200

    test

---
+++ build/sw.js
@@ -0,0 +1,43 @@
+
+const cacheName = 'Test';
+const resourcesToPrecache = [
+  'index.html',
+];
+
+
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
+);
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+url_not_2_cache = ["silk", "admin", "API", "accounts"];
+
+// ************** 
+// INSTALL EVENT
+// **************
+
+self.addEventListener('install', event => {
+  console.log("installed");
+  event.waitUntil(caches.open(cacheName)
+  .then(cache => {
+      return cache.addAll(resourcesToPrecache);
+  })
+  .then(self.skipWaiting())
+  );
+});
+
+workbox.routing.registerRoute(
+  ({ url }) => url.href.includes("API/user.json"),
+  new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerRoute(
+  ({ url }) => !url_not_2_cache.some((key) => url.href.includes(key)),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "SW-cache",
+  })
+);

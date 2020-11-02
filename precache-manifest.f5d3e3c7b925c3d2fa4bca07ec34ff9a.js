a11cb9423c266aa329aa69107624470a7eefddfb
Dror Tartakovsky <drortartak@gmail.com>
2020-11-02 12:43:39 +0200

    Updates

---
+++ precache-manifest.f5d3e3c7b925c3d2fa4bca07ec34ff9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d7129b7cdd47e79eabce32a60b6123",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "cf55a5f8f74e0afca70c",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ccf420c904bd3d9e4903",
+    "url": "/test/static/js/2.32f219f4.chunk.js"
+  },
+  {
+    "revision": "5c5a0857bb8747e9ae7db7917ecbbf79",
+    "url": "/test/static/js/2.32f219f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf55a5f8f74e0afca70c",
+    "url": "/test/static/js/main.a3897d03.chunk.js"
+  },
+  {
+    "revision": "6ed7f43a5afa7409cea0",
+    "url": "/test/static/js/runtime-main.11f499c5.js"
+  }
+]);
